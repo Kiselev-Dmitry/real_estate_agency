32106459ea5628ded28e7312da5b35512ed48022
Kiselev-Dmitry <kdv150275@gmail.com>
2023-11-04 19:33:47 +0300

    Add inline info for Flats and Owners

--- property/admin.py
+++ property/admin.py
@@ -3,6 +3,11 @@ from django.contrib import admin
 from .models import Flat, Complaint, Owner
 
 
+class OwnershipInline(admin.TabularInline):
+    model = Owner.flats_in_property.through
+    raw_id_fields = ("owner", "flat",)
+
+
 @admin.register(Flat)
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ("town", "address", "owner")
@@ -11,11 +16,13 @@ class FlatAdmin(admin.ModelAdmin):
     list_editable = ("new_building",)
     list_filter = ("new_building",)
     raw_id_fields = ("liked_by",)
+    inlines = [OwnershipInline]
 
 @admin.register(Owner)
 class OwnerAdmin(admin.ModelAdmin):
     raw_id_fields = ("flats_in_property",)
-#    exclude = ("flats_in_property",)
+    inlines = [OwnershipInline]
+    exclude = ("flats_in_property",)
 
 @admin.register(Complaint)
 class ComplaintAdmin(admin.ModelAdmin):
