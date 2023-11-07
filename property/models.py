517c2fdfe3f1842e2103bfe10c670d9f00c30259
Kiselev-Dmitry <kdv150275@gmail.com>
2023-11-07 21:24:03 +0300

    Upgrade related names

--- property/models.py
+++ property/models.py
@@ -73,13 +73,13 @@ class Complaint(models.Model):
         blank=True,
         null=True,
         verbose_name="Кто жаловался",
-        related_name="user_complains"
+        related_name="complaints"
     )
     flat = models.ForeignKey(
         Flat,
         on_delete=models.CASCADE,
         verbose_name="Квартира, на которую пожаловались",
-        related_name="flat_complaints"
+        related_name="complaints"
     )
     text = models.TextField(verbose_name="Текст жалобы", blank=True)
 
